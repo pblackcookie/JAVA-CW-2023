c16226f534d24ad22e3c359642e6a9b84abde339
YUXIN SUN <2029536227@qq.com>
2024-03-11 20:52:30 +0000

    Add recegonize the invaild file form(doesn't using the tab to seperate the each word)

--- cw-db/src/main/java/edu/uob/FileProcess.java
+++ cw-db/src/main/java/edu/uob/FileProcess.java
@@ -2,48 +2,60 @@ package edu.uob;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class FileProcess {
     private String databasePath;
-    private String filePath;
-
-    //DBServer dbServer = new DBServer();
-    //DatabaseProcess curDatabasePath = new DatabaseProcess();
 
     // Try to display all files by importing java.io package and using it.
     public void displayFiles(String databaseName) throws IOException{
-        //DBServer dbServer = new DBServer();
         DatabaseProcess curDatabasePath = new DatabaseProcess();
         databasePath = curDatabasePath.getDatabasePath(databaseName);
         System.out.println(databasePath);
         File documentsFolder = new File(databasePath);
         File[] documents = documentsFolder.listFiles();
-        for (File document : documents) {
-            if (document.isDirectory()) {
-                System.out.println("Folder: " + document.getName());
-            } else {
-                System.out.println("File: " + document.getName());
+        if (documents != null) {
+            for (File document : documents) {
+                if (document.isDirectory()) {
+                    System.out.println("Folder: " + document.getName());
+                } else {
+                    System.out.println("File: " + document.getName());
+                }
             }
         }
     }
 
     // For reading the stored files in the databases.
     public void readFileContent (String fileName, String databaseName) throws IOException{
-        //DBServer dbServer = new DBServer();
         DatabaseProcess curDatabasePath = new DatabaseProcess();
         databasePath = curDatabasePath.getDatabasePath(databaseName);
-        filePath = databasePath + File.separator + fileName;
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+        String filePath = databasePath + File.separator + fileName;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
+            int lineNumber = 0; // expression to check if the tab is correctly used
+            Pattern pattern = Pattern.compile("\\w+(?:\\t[\\w@]+)*");
+            lineNumber++;
             while((line = reader.readLine())  != null){
                 System.out.println(line);
+                Matcher matcher = pattern.matcher(line);
+                try{
+                    if (!matcher.matches()) {
+                        System.out.println(line);
+                        throw new IOException("Invalid format at line " + lineNumber);
+                    }
+                }catch(IOException ioe){
+                    System.err.println("Invalid format at line " + lineNumber + ". Fixing...");
+                    throw new IOException("Invalid format at line " + lineNumber);
+                }
             }
-        }catch (IOException e){
+        }catch (IOException ioe2){
             // If server can't found the file, then will create it first.
             try {
                 Files.createFile(Path.of(filePath));
-            }catch(IOException ioe) {
-                throw new IOException("Error reading file: " + ioe.getMessage());
+            }catch(IOException ioe3) {
+                throw new IOException("Error reading file: " + ioe3.getMessage());
             }
         }
     }
